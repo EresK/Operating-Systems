daf49d9301fcafab72a2c086bca96bf238b51309
Eres <e.kuular@g.nsu.ru>
2021-10-21 14:58:47 +0700

    lab 1

--- lab1/getopt_prog.c
+++ lab1/getopt_prog.c
@@ -7,14 +7,14 @@
 extern char* optarg;
 extern int optind, opterr, optopt;
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[], char* envp[])
 {
-    char* options = "ispuU:n";
+    char* options = "ispuU:vV:";
     char* U_ptr;
-    int c;
+    int c, k;
     long new_ulimit;
     
-    printf("argc equals %d\n", argc); // is it really nead?
+    printf("argc equals %d\n", argc); 
     while ((c = getopt(argc, argv, options)) != -1)
     {
         switch (c)
@@ -38,19 +38,33 @@ int main(int argc, char** argv)
                 printf("maximum file size: %ld\n", ulimit(UL_GETFSIZE));
                 break;
             
-            /*case 'U':
-                if(new_ulimit = atol(optarg))
-                    perror("Error: can not atol argument\n");
+            case 'U':
+                if((new_ulimit = atol(optarg)) <= 0)
+                    fprintf(stderr, "Error: %s\n", strerror(errno));
                 else
                 {
                     if(ulimit(UL_SETFSIZE, new_ulimit) == -1)
-                        fprintf("Error: %s\n", strerror(errno));
+                        fprintf(stderr, "Error: %s\n", strerror(errno));
                     else
                         printf("success SETFSIZE ulimit\n");
                 }
-                break;*/
-            case 'U':
-                U_ptr = optarg;
+                break;
+
+            case 'v':
+                k = 0;
+                while (envp[k] != 0)
+                {
+                    printf("%s\n", envp[k]);
+                    k++;
+                }
+                
+                break;
+
+            case 'V':
+                if(putenv(optarg))
+                    fprintf(stderr, "Error: %s\n", strerror(errno));
+                else
+                    printf("success putenv\n");
                 break;
 
             case '?':
