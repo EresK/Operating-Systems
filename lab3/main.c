97ebe111181b672a48b0836c5ec5b79c14833205
Eres <e.kuular@g.nsu.ru>
2022-01-17 22:20:50 +0700

    add lab 3

--- lab3/main.c
+++ lab3/main.c
@@ -1,42 +1,30 @@
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-void openFile(char* name)
-{
-        FILE* file = fopen(name, "rw");
-
-        if (!file)
-            perror("Error: can not open file\n");
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        if (setuid(getuid()))
+            perror("setuid failed\n");
         else
-            printf("File \"%s\" opened\n", name);
-
-        fclose(file);
-}
-
-int main(int argc, char* argv[])
-{
-    printf("Real ID: %u\nEffective ID: %u\n", getuid(), geteuid());
-
-    if (argc >= 2)
-        openFile(argv[1]);
-    else
-    {
-        perror("Error: enter file name\n");
-        exit(1);
+            printf("setuid succeed\n");
     }
 
-    if(setuid(getuid()))
-        fprintf(stderr, "Error: failed setuid \"%s\"\n", strerror(errno));
-    else
-        printf("Success: setuid\n");
+    printf("uid: %d\neuid: %d\n", getuid(), geteuid());
 
-    printf("Real ID: %u\nEffective ID: %u\n", getuid(), geteuid());
+    FILE* file;
 
-    openFile(argv[1]);
+    if ((file = fopen("text", "rw")) != NULL) {
+        printf("File opened\n");
+        fclose(file);
+        printf("File closed\n");
+    }
+    else {
+        perror("fopen failed\n");
+        exit(1);
+    }
 
     return 0;
 }
