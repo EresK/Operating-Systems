06d6b554448742874be2a54307b44a75667114b4
Eres <e.kuular@g.nsu.ru>
2022-01-17 17:54:29 +0700

    add lab 11

--- lab11/myexecvpe.c
+++ lab11/myexecvpe.c
@@ -1,20 +1,29 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h> // execve
 #include <libgen.h> // pathfind
 
 int myexecvpe(char* filename, char* argv[], char* envp[])
 {
-    char* env_path;
-    if ((env_path = getenv("PATH")) != NULL)
+    if (strchr(filename, (int)'/') != NULL && !access(filename, X_OK))
     {
-        char* path = pathfind(env_path, filename, "rx");
-
-        if (path != NULL && !access(path, F_OK))
+        if (execve(filename, argv, envp))
+            return -1;
+    }
+    else
+    {
+        char* env_path;
+        if ((env_path = getenv("PATH")) != NULL)
         {
-            if (execve(path, argv, envp))
-                return -1;
+            char* path = pathfind(env_path, filename, "x");
+
+            if (path != NULL)
+            {
+                if (execve(path, argv, envp))
+                    return -1;
+            }
         }
     }
 
