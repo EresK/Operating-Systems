f8f0b6ef722522f255769cab617cdab552f0972e
Eres <e.kuular@g.nsu.ru>
2022-01-28 14:30:54 +0700

    add lab8

---
+++ lab8/advisory/main_a.c
@@ -0,0 +1,47 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int fd;
+
+    if ((fd = open("file", O_RDWR)) != -1) {
+        struct flock lock;
+
+        lock.l_type = F_WRLCK;
+        lock.l_whence = SEEK_SET;
+        lock.l_start = 0;
+        lock.l_len = 0;
+
+        if (fcntl(fd, F_SETLK, &lock) != -1) {
+            if (system("nano file") == -1) {
+                perror("system() failed\n");
+                exit(3);
+            }
+        }
+        else {
+            perror("fcntl failed\n");
+            exit(2); // save
+        }
+
+        lock.l_type = F_UNLCK;
+        if (fcntl(fd, F_SETLK, &lock) == -1) {
+            perror("Can not unlock file\n");
+            exit(4);
+        }
+
+        if (close(fd) == -1) {
+            perror("Can not close file\n");
+            exit(5);
+        }
+    }
+    else {
+        perror("Can not open file\n");
+        exit(1);
+    }
+
+    return 0;
+}
