5b2b2a04a7d0fa96dbc4bea7ebea03defa6dccd9
Eres <e.kuular@g.nsu.ru>
2022-02-11 22:30:41 +0700

    rewrite lab 3

--- lab3/main.c
+++ lab3/main.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[]) {
-    if (argc > 1) {
-        if (setuid(getuid()))
-            perror("setuid failed\n");
-        else
-            printf("setuid succeed\n");
-    }
-
-    printf("uid: %d\neuid: %d\n", getuid(), geteuid());
-
+int main() {
     FILE* file;
 
-    if ((file = fopen("text", "rw")) != NULL) {
+    printf("uid: %u\neuid: %u\n", getuid(), geteuid());
+    if ((file = fopen("data_file", "rw")) != NULL) {
         printf("File opened\n");
         fclose(file);
         printf("File closed\n");
     }
     else {
-        perror("fopen failed\n");
+        perror("Error: fopen data_file failed\n");
         exit(1);
     }
+
+    setuid(getuid());
+
+    printf("uid: %u\neuid: %u\n", getuid(), geteuid());
+    if ((file = fopen("data_file", "rw")) == NULL) {
+        printf("Can not open data_file after setuid()\n");
+    }
+    else
+        fclose(file);
 
     return 0;
 }
