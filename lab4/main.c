00cde65180e998b0fddcdd7fbae7875568a7062d
Eres <e.kuular@g.nsu.ru>
2022-02-11 22:51:58 +0700

    fix lab 4

--- lab4/main.c
+++ lab4/main.c
@@ -2,37 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main () {
+int main() {
+    char* strlist[BUFSIZ];
+    int count = 0;
 
-    char* lines[BUFSIZ] = {0};
-    int cnt = 0;
-
-    char buff[BUFSIZ];
+    char line[BUFSIZ];
     size_t length;
 
-    while (fgets(buff, BUFSIZ, stdin) != NULL && (cnt < BUFSIZ))
-    {
-        length = strlen(buff);
-
-        if (length > 0 && buff[0] == '.') {
+    while (fgets(line, BUFSIZ, stdin) != NULL && (count < BUFSIZ)) {
+        length = strlen(line);
+        if (length > 0 && line[0] == '.') {
             break;
         }
         else if (length > 0) {
-            char* ptr;
+            char* pt;
 
-            if ((ptr = malloc((length + 1) * sizeof(char))) == NULL) {
-                perror("Can not allocate memory");
+            if ((pt = malloc(length + 1)) == NULL) {
+                perror("Error: can not allocate memory");
                 break;
             }
-            strcpy(ptr, buff);
-
-            lines[cnt++] = ptr;
+            strcpy(pt, line);
+            strlist[count++] = pt;
         }
     }
 
-    for (int i = 0; (lines[i] != NULL) && (i < BUFSIZ); i++) {
-        printf("%s", lines[i]);
-        free(lines[i]);
+    for (int i = 0; i < count; i++) {
+        printf("%s", strlist[i]);
+        free(strlist[i]);
     }
 
     return 0;
